5dc47da0a99f0c7db85c3bdcf0ee8ea7fb3452b7
SohamDesai32 <202003021.sohamdak@student.xavier.ac.in>
2022-09-11 00:08:38 +0530

    Testing nextjs and routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        This is a sample text
+    </div>
+  )
+}
+
+export default about
---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const contact = () => {
+  return (
+    <div>
+      <ul>
+        <li>ph no - 9833793411</li>
+        <li>address - Kandivalli</li>
+      </ul>
+    </div>
+  );
+};
+
+export default contact;
